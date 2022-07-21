a1d95835b948457f60c620a59e641476398e1783
Divyam Agrawal <agrawaldivyam2001@gmail.com>
2022-07-22 01:18:23 +0530

    Completed Login form fields

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,28 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './login.css'
 import {Link} from 'react-router-dom'
+
 const Login = () => {
 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password)
+  }
 
   return (
     <div className='login section__padding'>
       <div className="login-container">
         <h1>Login</h1>
         <form className='login-writeForm' autoComplete='off'>
-          <div className="login-formGroup">
-            <label>Username</label>
-            <input type="text" placeholder='Username'  />
+        <div className="login-formGroup">
+            <label>Email</label>
+            <input type="email" required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div className="login-formGroup">
             <label>Password</label>
-            <input type="password" placeholder='Password'  />
+            <input type="password" required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
           
          <div className="login-button">
-          <button className='login-writeButton' type='submit'>Login</button>
           <Link to="/register">
-            <button className='login-reg-writeButton' type='submit'>Register</button>
+            <button className='login-reg-writeButton'>register</button>
           </Link>
+          <button className='login-writeButton' onClick={handleSubmit}>login</button>
          </div>
         </form>
       </div>
