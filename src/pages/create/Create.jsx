09233b1d64f71ca5c3cf80ebca3a484402c57b0d
Divyam Agrawal <agrawaldivyam2001@gmail.com>
2022-07-22 16:31:18 +0530

    Added Mint Warranty NFT UI

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,62 +1,60 @@
+import React, {useState, useEffect} from "react";
 import './create.css'
-import Image from '../../assets/Image.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
 const Create = () => {
+
+  const [items, setItems] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    const keyDownHandler = event => {
+      if (event.key === 'Enter') {
+        event.preventDefault();
+        handleAddButtonClick();
+      }
+    };
+
+    document.addEventListener('keydown', keyDownHandler);
+    return () => {
+      document.removeEventListener('keydown', keyDownHandler);
+    };
+  }, [inputValue]);  
+
+  const handleAddButtonClick = () => {
+    console.log(inputValue)
+		const newItem = {
+			itemName: inputValue,
+			quantity: 1,
+			isSelected: false,
+		};
+
+		const newItems = [...items, newItem];
+
+		setItems(newItems);
+		setInputValue('');
+	};
 
   return (
     <div className='create section__padding'>
       <div className="create-container">
-        <h1>Create new Item</h1>
-        <p className='upload-file'>Upload File</p>
-        <div className="upload-img-show">
-            <h3>JPG, PNG, GIF, SVG, WEBM, MP3, MP4. Max 100mb.</h3>
-            <img src={Image} alt="banner" />
-            <p>Drag and Drop File</p>
-        </div>
-        <form className='writeForm' autoComplete='off'>
-          
-          <div className="formGroup">
-            <label>Upload</label>
-            <input type="file" className='custom-file-input'
-          />
-          </div>
-          <div className="formGroup">
-            <label>Name</label>
-            <input type="text" placeholder='Item Name' autoFocus={true} />
-          </div>
-          <div className="formGroup">
-            <label>Description</label>
-            <textarea type="text" rows={4}
-          placeholder='Decription of your item' 
-          ></textarea>
-          </div>
-          <div className="formGroup">
-            <label>Price</label>
-            <div className="twoForm">
-              <input type="text" placeholder='Price'  />
-              <select>
-                <option value="ETH">ETH</option>
-                <option value="BTC">BTC</option>
-                <option value="LTC">LTC</option>
-              </select>
+        <h1>Mint Warranty NFTs</h1>
+        <div className='add-item-box'>
+					<input value={inputValue} onChange={(e) => setInputValue(e.target.value)} 
+          className='add-item-input' placeholder='Add product serial number...' />
+					<FontAwesomeIcon icon={faPlus} onClick={() => handleAddButtonClick()} />
+				</div>
+        <div className='item-list'>
+          {items.map((item, index) => (
+            <div className='item-container'>
+              <div className='item-name'>
+                <span>{item.itemName}</span>
+              </div>
             </div>
-          </div>
-          <div className="formGroup">
-            <label>Category</label>
-            <select >
-               <option>Art</option>
-               <option>Photography</option>
-               <option>Sports</option>
-               <option>Collectibles</option>
-               <option>Trading Cards</option>
-               <option>Utility</option>
-            </select>
-          </div>
-          <div className="formGroup">
-            <label>Available Items</label>
-            <input type="text" placeholder='No of Items'/>
-          </div>
-          <button className='writeButton'>Create Item</button>
-        </form>
+          ))}
+        </div>
+        <button className='mint-button'>Mint all Items</button>
       </div>
     </div>
    
