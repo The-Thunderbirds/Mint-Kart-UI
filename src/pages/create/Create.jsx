693e1f9af3e7a821cd51cf9e6fd1ecaf4f79a908
Divyam Agrawal <agrawaldivyam2001@gmail.com>
2022-07-28 18:29:22 +0530

    Added UI for Minting Queue

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -6,17 +6,18 @@ import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import { useSnackbar } from 'notistack';
-import { useDispatch, useSelector } from 'react-redux';
 import axios from "axios";
+import { Link } from 'react-router-dom';
+import Product from './Product';
+import Slider from 'react-slick';
+import { NextBtn, PreviousBtn } from '../../components';
 
 const Create = () => {
 
-  const dispatch = useDispatch();
   const { enqueueSnackbar } = useSnackbar();
-
-  const { user, loading, isAuthenticated, error } = useSelector((state) => state.user);
-
+  
   const [serialNums, setSerialNums] = useState([]);
+  const [products, setProducts] = useState([]);
   const [inputValue, setInputValue] = useState('');
   const [password, setPassword] = useState('');
 
@@ -43,12 +44,27 @@ const Create = () => {
 
   const handleShow = () => setShow(true);
 
+  const handleAddButtonClick = async () => {
+    try {
+      const { data } = await axios.get(`/api/v1/product/serial/${inputValue}`);
 
-  const handleAddButtonClick = () => {
+      // Checking for duplicate serial numbers
+      // for(let i = 0; i < serialNums.length; i++) { 
+      //   if(serialNums[i] === inputValue) {
+      //     enqueueSnackbar("Product with given serial number already exists", { variant: "error" });
+      //     return;
+      //     }
+      // }
 
-    const newSerialNums =  [...serialNums, inputValue];
-    setSerialNums(newSerialNums);
-		setInputValue('');
+      const newSerialNums =  [...serialNums, inputValue];
+      const newProducts = [...products, data.product];
+      setSerialNums(newSerialNums);
+      setProducts(newProducts);
+      setInputValue('');
+    } catch (error) {
+      enqueueSnackbar(error.response.data.message, { variant: "error" });
+      return;
+    }    
 	};
 
   const handleMint = async () => {
@@ -68,23 +84,41 @@ const Create = () => {
   }
 
   return (
-    <div className='create section__padding'>
+    <div className='create create_section__padding'>
       <div className="create-container">
-        <h1>Mint Warranty NFTs</h1>
+        <h1 className="create-container-title">Mint Warranty NFTs</h1>
         <div className='add-item-box'>
 					<input value={inputValue} onChange={(e) => setInputValue(e.target.value)} 
           className='add-item-input' placeholder='Add product serial number...' autoFocus/>
 					<FontAwesomeIcon icon={faPlus} onClick={() => handleAddButtonClick()} />
 				</div>
-        <div className='item-list'>
-          {serialNums.map(serialNum => (
-            <div key={serialNum} className='item-container'>
+        {products.length !== 0 && (
+        <section className="w-full shadow overflow-hidden">
+            <div className="flex px-6 py-4 justify-between items-center">
+                <h1 className="text-xl font-medium">Minting Queue</h1>
+            </div>            
+            <hr />
+            {/* <!-- header --> */}
+            <Slider {...settings}>
+                {products.map((item, i) => (
+                    <Product {...item[0]} key={i} />
+                ))}
+            </Slider>
+        </section>
+        )}
+
+        {/* <div className='item-list'>
+          {products.map(product => (
+            <div key={product[0].serialNumber} className='item-container'>
+              {console.log(product[0])}
               <div className='item-name'>
-                <span>{serialNum}</span>
+                <a target="_blank" href={`/post/${product[0].serialNumber}`}>
+                  <span>{product[0].serialNumber}</span>
+                </a>
               </div>
             </div>
           ))}
-        </div>
+        </div> */}
         <button className='mint-button' onClick={handleShow}>Mint all Items</button>
       </div>
       <Modal show={show} onHide={handleClose} centered>
@@ -114,6 +148,41 @@ const Create = () => {
       </Modal>
     </div>   
   )
+};
+
+const settings = {
+  dots: false,
+  infinite: false,
+  speed: 500,
+  slidesToShow: 6,
+  slidesToScroll: 6,
+  initialSlide: 1,
+  swipe: false,
+  prevArrow: <PreviousBtn />,
+  nextArrow: <NextBtn />,
+  responsive: [
+      {
+          breakpoint: 1024,
+          settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3
+          }
+      },
+      {
+          breakpoint: 600,
+          settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+          }
+      },
+      {
+          breakpoint: 480,
+          settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+          }
+      }
+  ]
 };
 
 export default Create;
---
+++ src/pages/create/Product.jsx
@@ -0,0 +1,16 @@
+const Product = ({ images, name, serialNumber, warranty }) => {
+    console.log(images)
+    return (
+        <a target="_blank" href={`/post/${serialNumber}`}
+            className="flex flex-col items-center gap-1.5 p-6 cursor-pointer">
+            <div className="w-36 h-36 transform hover:scale-110 transition-transform duration-150 ease-out">
+                <img draggable="false" className="w-full h-full object-contain" src={images[0].url} alt={name} />
+            </div>
+            <h2 className="font-medium text-sm mt-2">{name.substr(0, 30)}...</h2>
+            <span className="text-primary-green text-sm">{serialNumber.substr(0, 15)}...</span>
+            <span className="text-gray-500 text-sm">{warranty} year warranty</span>
+        </a>
+    );
+};
+
+export default Product;
