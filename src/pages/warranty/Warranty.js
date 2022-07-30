2ceaab238cbeffc9ba1b12e0844092fd5af5674e
Divyam Agrawal <agrawaldivyam2001@gmail.com>
2022-07-30 23:09:35 +0530

    Updated state variables

--- src/pages/warranty/Warranty.js
+++ src/pages/warranty/Warranty.js
@@ -6,31 +6,57 @@ import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import axios from "axios";
+import { useSnackbar } from 'notistack';
+import CircularProgress from '@mui/material/CircularProgress';
+import { BackdropLoader } from '../../components';
 
 const Warranty = () => {
   
-  const myRef = useRef(null)
+  const myRef = useRef(null);
+  const { enqueueSnackbar } = useSnackbar();
 
   const [serialNum, setSerialNum] = useState('');
   const [user_email, setUserEmail] = useState('');
-  const [hasClickedCheck, setHasClickedCheck] = useState(false);
-  const [hasNFT, setHasNFT] = useState(false);
+
+  const [checkLoading, setCheckLoading] = useState(false);  
+
+  const [customer, setCustomer] = useState();
+  const [order, setOrder] = useState();
+  const [product, setProduct] = useState();
+  const [nftId, setNftId] = useState("");
   const [inWarranty, setInWarranty] = useState(false);
+  const [warrantyDaysLeft, setWarrantyDaysLeft] = useState(0);
+
   const [currSNum, setCurrSNum] = useState('');
   const [newSNum, setNewSNum] = useState('');
 
   const handleCheckClick = async () => {
-    setHasClickedCheck(true);
+    setCustomer(null);
+    setOrder(null);
+    setProduct(null);
+    setNftId("");
+    setCheckLoading(true);
 
-    const { data } = await axios.post(
-      `/api/v1/fetch-warranty`,
-      {serialNum, user_email}
-    );
-
-    console.log(data);
-    
-    setHasNFT(true);
-    myRef.current.scrollIntoView();
+    try {
+      const { data } = await axios.post(
+        `/api/v1/fetch-warranty`,
+        {serialNum, user_email}
+        );
+  
+      console.log(data);
+      if(data.success) {
+        setCustomer(data.user);
+        setOrder(data.order);
+        setProduct(data.product);
+        setNftId(data.product.nft_id);
+        calculateWarrantyLeft(data.order.createdAt, data.product.warranty);
+        myRef.current.scrollIntoView();
+      }
+      
+    } catch(error) {
+      enqueueSnackbar(error.response.data.message, { variant: "error" });
+    }
+    setCheckLoading(false);  
   }
 
   const [show, setShow] = useState(false);
@@ -42,8 +68,22 @@ const Warranty = () => {
 
   }
 
+  const calculateWarrantyLeft = (createdAt, warranty) => {
+    const today  = new Date();
+    const purchasedOn = new Date(createdAt);
+    const diffDays = parseInt((today - purchasedOn) / (1000 * 60 * 60 * 24), 10);
+    if(diffDays > (warranty * 365)) {
+      setInWarranty(false);
+    }
+    else {
+      setInWarranty(true);
+      setWarrantyDaysLeft((warranty * 365) - diffDays);
+    }
+  } 
+
   return (
     <>
+    {checkLoading && <BackdropLoader />}
     <div className='create create_section__padding'>
       <div className="create-container">
         <h1 className="create-container-title">Check Product's Warranty</h1>
@@ -60,8 +100,8 @@ const Warranty = () => {
     </div>   
 
     <main className="mt-12 sm:mt-0" ref={myRef}>
-      {hasClickedCheck && !hasNFT? <div className="not-exists"><h1>NFT does not exist</h1></div> : 
-      hasClickedCheck && hasNFT? (
+      {product != null && nftId === "" ? <div className="not-exists"><h1>NFT does not exist</h1></div> : 
+      product != null && nftId !== ""? (
         <>
         {/* <!-- product image & description container --> */}
         <div className="w-full flex flex-col sm:flex-row sm:p-2 relative text-white">
@@ -72,7 +112,7 @@ const Warranty = () => {
                 <div className="flex flex-col gap-3 m-3">
                     <div className="w-full h-full pb-6 border-[#a5a5a5]/[.1] relative">
                         <img draggable="false" className="w-full h-96 object-contain" 
-                        src="https://res.cloudinary.com/dgrr4aftk/image/upload/v1658843834/products/v6t5zwancyl1gqpjgprg.webp" alt={"Product image"}/>
+                        src={product.images[0].url} alt={product.name}/>
                     </div>
                 </div>
                 {/* <!-- imgbox --> */}
@@ -85,42 +125,85 @@ const Warranty = () => {
                 {/* <!-- whole product description --> */}
                 <div className="flex flex-col gap-2 mb-4">
 
-                    <h2 className="text-xl">{"product[0].name"}</h2>
+                    <h2 className="text-xl">{product.name}</h2>
                     {inWarranty ? 
                     <h3 className="text-m text-green-400">{"In Warranty"}</h3> :
                     <h3 className="text-m text-rose-600">{"Out of Warranty"}</h3> 
                     }
                   {/* <!-- warranty & brand --> */}
                     <div className="flex gap-8 mt-2 items-center text-sm">
-                        <img draggable="false" className="w-20 h-8 p-0.5 object-contain" 
-                        src={"https://res.cloudinary.com/dgrr4aftk/image/upload/v1658843836/brands/pv2ltml2ggohciedcw6j.jpg"} alt={"product[0].brand && product[0].brand.name"} />
-                        <span>{"product[0].warranty"} Year Warranty </span>
+                        <img draggable="false" className="w-20 h-8 p-0.5" 
+                        src={product.brand.logo.url} alt={product.brand.name} />
+                        <span>{product.warranty} Year Warranty </span>
                     </div>
                     {/* <!-- warranty & brand --> */}
 
+
+                    {/* <!-- customer details --> */}
+                    <div className="flex flex-col sm:flex-row justify-between">
+                        <div className="flex gap-16 mt-4 items-stretch text-sm">
+                            <p className="text-neutral-400 font-medium">Customer Details</p>
+
+                            <ul className="list-disc flex flex-col gap-2 w-64">
+                              <li>
+                                <p>{"Name- "} 
+                                <span>{customer.name}</span>
+                                </p>
+                              </li>
+                              <li>
+                                <p>{"Email- "} 
+                                <span>{customer.email}</span>
+                                </p>
+                              </li>
+                              <li>
+                                <p>{"Phone number- "} 
+                                <span>{customer.mobile_number}</span>
+                                </p>
+                              </li>
+                            </ul>
+                        </div>
+                    </div>
+                    {/* <!-- customer details --> */}
+
                     {/* <!-- purchase details --> */}
                     <div className="flex flex-col sm:flex-row justify-between">
-                        {/* <!-- highlights details --> */}
                         <div className="flex gap-16 mt-4 items-stretch text-sm">
                             <p className="text-neutral-400 font-medium">Purchase Details</p>
 
                             <ul className="list-disc flex flex-col gap-2 w-64">
                               <li>
-                                <p>{"Purchased on: "}
-                                <span>{"5 July 2022"}</span>
+                                <p>{"Purchased on- "} 
+                                <span>{new Date(order.createdAt).toLocaleDateString("en-US", options)}</span>
                                 </p>
                               </li>
+                              {inWarranty &&
                               <li>
-                                <p>{"Warranty left: "}
-                                <span>{"10 days"}</span>
+                                <p>{"Warranty left- "}
+                                <span>{warrantyDaysLeft} days</span>
+                                </p>
+                              </li>
+                              }
+                            </ul>
+                        </div>
+                    </div>
+                    {/* <!-- purchase details --> */}
+
+                    {/* <!-- nft details --> */}
+                    <div className="flex flex-col sm:flex-row justify-between">
+                        <div className="flex gap-16 mt-4 items-stretch text-sm">
+                            <p className="text-neutral-400 font-medium">NFT Details &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+
+                            <ul className="list-disc flex flex-col gap-2 w-64">
+                              <li>
+                                <p>{"NFT ID- "}
+                                <span>{nftId}</span>
                                 </p>
                               </li>
                             </ul>
                         </div>
-                        {/* <!-- purchase details --> */}
-
                     </div>
-                    {/* <!-- highlights & services details --> */}
+                    {/* <!-- nft details --> */}
+
 
                     {/* <!-- specifications border box --> */}
                     <div className="w-full mt-4 pb-4 rounded-sm border flex flex-col">
@@ -185,5 +268,7 @@ const Warranty = () => {
     </>
   )
 };
+
+const options = { year: 'numeric', month: 'long', day: 'numeric' };
 
 export default Warranty;
