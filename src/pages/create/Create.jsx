58d4af8935581aeadff053bf7e759e8505de5350
Divyam Agrawal <agrawaldivyam2001@gmail.com>
2022-07-31 18:59:53 +0530

    Uncommented checking for duplicate serial number

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -58,15 +58,15 @@ const Create = () => {
         const { data } = await axios.get(`/api/v1/product/serial/${id}`);
   
         // Checking for duplicate serial numbers (not working properly)
-        // const hasDuplicate = false;
-        // for(let j = 0; j < serialNums.length; j++) { 
-        //   if(serialNums[j] === id) {
-        //      enqueueSnackbar(`Product with given serial number ${id.substr(0, 10)}... already exists`, { variant: "error" });
-        //      hasDuplicate = true;
-        //     //  break;
-        //   }
-        // }
-        // if(hasDuplicate) continue;
+        let hasDuplicate = false;
+        for(let j = 0; j < serialNums.length; j++) { 
+          if(serialNums[j] === id) {
+             enqueueSnackbar(`Product with given serial number ${id.substr(0, 10)}... already exists`, { variant: "error" });
+             hasDuplicate = true;
+             break;
+          }
+        }
+        if(hasDuplicate) continue;
   
         const newSerialNums = serialNums;
         newSerialNums.push(id);
